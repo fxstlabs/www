c4fd02f89feee62abb8866907e4f8ca0d68773ea
ayimany <ayimany.dev@proton.me>
2026-01-14 18:09:03 -0600

    Handle default translations when no CDN is present

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,14 @@
 import {getRequestConfig} from "next-intl/server";
 import {hasLocale} from 'next-intl';
 import {routing} from "@/i18n/routing";
-import {fxstCdn} from "@/resources/cdn";
+import {fxstCdn, isCdnUp} from "@/resources/cdn";
 import {cache} from "react";
 
 const getDictionary = cache(async (locale: string) => {
+    if (!await isCdnUp()) {
+        return (await import('@/resources/en.json')).default
+    }
+
     const url = fxstCdn(`/static/dictionaries/${locale}.json`)
     const res = await fetch(url, {
         next: {
